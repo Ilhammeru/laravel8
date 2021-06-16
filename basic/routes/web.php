8e8e49ce655bd40564338a38afdf41458f6cca1c
Ilham Gumilang <willyagungrohmana@ilham-Macbook.local>
2021-06-17 02:03:35 +0700

    upadate belajar laravel8

--- basic/routes/web.php
+++ basic/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\contactController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/contact', [contactController::class, 'index'])->middleware('age');
+
+Route::get('/about', function() {
+    return view('admin/about');
 });
